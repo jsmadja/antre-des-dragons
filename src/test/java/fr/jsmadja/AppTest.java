68fe57a290ecda99053973c1598d2ce97acc7fac
Julien Smadja <julien.smadja@gmail.com>
2019-03-16 11:36:44 +0100

    Initial commit

---
+++ src/test/java/fr/jsmadja/AppTest.java
@@ -0,0 +1,14 @@
+package fr.jsmadja;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
