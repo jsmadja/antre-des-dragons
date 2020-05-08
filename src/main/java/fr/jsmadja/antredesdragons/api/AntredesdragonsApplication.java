2980dc3f2e35d41cfeddfe43111dcaa5f6b6cc04
Julien Smadja <julien.smadja@gmail.com>
2020-05-08 20:58:59 +0200

    Add Spring

---
+++ src/main/java/fr/jsmadja/antredesdragons/api/AntredesdragonsApplication.java
@@ -0,0 +1,13 @@
+package fr.jsmadja.antredesdragons.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntredesdragonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntredesdragonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/jsmadja/antredesdragons/api/AntredesdragonsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jsmadja.antredesdragons.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntredesdragonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
